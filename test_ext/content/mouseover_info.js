b5bf0b9cf12608e44bc11f1f6985e97d13b44a9e
shan0325 <shan0325@naver.com>
2023-05-26 00:35:33 +0900

    add mouse scroll event

--- test_ext/content/mouseover_info.js
+++ test_ext/content/mouseover_info.js
@@ -5,35 +5,8 @@ const data = {
   tooltipId: "chromeExtTooltip",
   tooltipZoomBtnId: "chromeExtTooltipZoomBtn",
   tooltipContentId: "chromeExtTooltipContent",
+  tooltipLastTop: 100,
 };
-
-// 마우스 오버 이벤트를 감지하여 엘리먼트 정보를 표시하는 함수
-function handleMouseOver(event) {
-  const target = event.target; // 이벤트가 발생한 엘리먼트 가져오기
-
-  const elementInfo = getElementInfo(target); // 엘리먼트 정보 가져오기
-
-  data.tooltip.style.display = "block";
-  data.tooltip.childNodes[1].textContent = elementInfo;
-
-  if (target.id === data.tooltipId || target.parentNode.id === data.tooltipId) {
-    data.tooltip.childNodes[1].textContent = "😗🫓🍧";
-  }
-}
-
-// tooltip을 이동할 수 있도록 하는 함수
-function startDrag(event) {
-  data.tooltipIsDragging = true;
-
-  const tooltipRect = data.tooltip.getBoundingClientRect();
-  data.tooltipOffset.x = event.clientX - tooltipRect.left;
-  data.tooltipOffset.y = event.clientY - tooltipRect.top;
-}
-
-function moveTooltip(pageX, pageY) {
-  data.tooltip.style.left = `${pageX - data.tooltipOffset.x}px`;
-  data.tooltip.style.top = `${pageY - data.tooltipOffset.y}px`;
-}
 
 // tooltip 엘리먼트를 생성하는 함수
 function createTooltip() {
@@ -75,24 +48,58 @@ function createTooltip() {
   return tooltip;
 }
 
+// 마우스 오버 이벤트를 감지하여 엘리먼트 정보를 표시하는 함수
+function handleMouseOver(event) {
+  if (data.tooltipIsDragging) return;
+
+  const target = event.target; // 이벤트가 발생한 엘리먼트 가져오기
+  const elementInfo = getElementInfo(target); // 엘리먼트 정보 가져오기
+
+  data.tooltip.style.display = "block";
+  data.tooltip.childNodes[1].innerHTML = elementInfo;
+  data.tooltip.childNodes[1].style.height = "";
+
+  if (target.id === data.tooltipId || target.parentNode.id === data.tooltipId) {
+    document.removeEventListener("mouseover", handleMouseOver);
+    data.tooltip.childNodes[1].textContent = "😗🫓🍧🏜️🥰🎈";
+    // data.tooltip.childNodes[1].innerHTML = "<img src='https://picsum.photos/150/150' />";
+    data.tooltip.childNodes[1].style.height = "100px";
+  }
+}
+
+// tooltip을 이동할 수 있도록 하는 함수
+function startDrag(event) {
+  data.tooltipIsDragging = true;
+
+  const tooltipRect = data.tooltip.getBoundingClientRect();
+  data.tooltipOffset.x = event.clientX - tooltipRect.left;
+  data.tooltipOffset.y = event.clientY - tooltipRect.top;
+}
+
+function moveTooltip(event) {
+  data.tooltip.style.left = `${event.pageX - data.tooltipOffset.x}px`;
+  data.tooltip.style.top = `${event.pageY - data.tooltipOffset.y}px`;
+  data.tooltipLastTop = data.tooltip.getBoundingClientRect().top;
+}
+
 // 엘리먼트 정보를 반환하는 함수
 function getElementInfo(element) {
-  let info = `<${element.tagName}>`;
+  let info = `<p>&lt;${element.tagName}&gt;<p>`;
   if (element.className) {
-    info += `\nClass: ${element.className}`;
+    info += `<p>Class: ${element.className}</p>`;
   }
   if (element.id) {
-    info += `\nId: ${element.id}`;
+    info += `<p>Id: ${element.id}</p>`;
   }
 
   // 이벤트 정보 가져오기
   const eventNames = Object.keys(element).filter((key) => key.startsWith("on"));
   if (eventNames.length > 0) {
-    info += "\n\nEvents:";
+    info += "<p>Events:</p>";
     eventNames.forEach((eventName) => {
       const handler = element[eventName];
       if (typeof handler === "function") {
-        info += `\n${eventName}: ${handler.name || "anonymous function"}`;
+        info += `<p>${eventName}: ${handler.name || "anonymous function"}</p>`;
       }
     });
   }
@@ -114,7 +121,7 @@ data.tooltip.addEventListener("mousedown", startDrag);
 // 이동 이벤트 리스너 등록
 document.addEventListener("mousemove", (event) => {
   if (data.tooltipIsDragging) {
-    moveTooltip(event.pageX, event.pageY);
+    moveTooltip(event);
   }
 });
 
@@ -123,18 +130,27 @@ document.addEventListener("mouseup", () => {
   data.tooltipIsDragging = false;
 });
 
-// 툴팁 숨김 이벤트 리스너 등록
-document.addEventListener("mouseout", function (event) {
-  if (event.target.id === data.tooltipId) {
-    // 툴팁 숨김
-    data.tooltip.style.display = "none";
-  }
+// tooltip 마우스 아웃 시 이벤트 리스너 다시 등록
+data.tooltip.addEventListener("mouseout", () => {
+  document.addEventListener("mouseover", handleMouseOver);
 });
 
+// 스크롤 시 tooltip 같이 이동
+window.addEventListener("scroll", () => {
+  data.tooltip.style.top = `${data.tooltipLastTop + window.scrollY}px`;
+});
+
+// 축소 버튼 클릭 시
 data.tooltip.childNodes[0].addEventListener("click", () => {
   if (data.tooltip.classList.contains("tooltip-small")) {
     data.tooltip.classList.remove("tooltip-small");
+    data.tooltip.style.width = "200px";
+    data.tooltip.style.height = "";
+    data.tooltip.childNodes[1].style.display = "block";
   } else {
     data.tooltip.classList.add("tooltip-small");
+    data.tooltip.style.width = "20px";
+    data.tooltip.style.height = "20px";
+    data.tooltip.childNodes[1].style.display = "none";
   }
 });
